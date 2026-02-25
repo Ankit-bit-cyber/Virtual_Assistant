c517ab85b3d7fdd440e5e8774a0944b4dcac3bff
Ankit-bit-cyber <208ankitkumarssm@gmail.com>
2026-02-25 15:27:48 +0530

    feat: enhance user assistant functionality with image upload and command processing

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,7 +1,6 @@
-
 import genToken from "../config/token.js"
 import User from "../models/user.model.js"
-
+import bcrypt from "bcryptjs"
 export const signUp=async (req,res)=>{
 try {
     const {name,email,password}=req.body
